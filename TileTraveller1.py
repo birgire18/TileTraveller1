398e16c294b1ef1d1028652bdef82f84b9c16a3b
Birgir Steinn Ellingsen <birgirsteinnellingsen@Birgirs-MacBook-Pro.local>
2018-09-18 09:42:29 +0000

    algorithm added try2

---
+++ TileTraveller1.py
@@ -0,0 +1,5 @@
+#First we create the file map from 1,1 to 3,3
+#Then make possible moves for each tile
+#Request the user for an input, wich should be a destination
+#Respond to that input.(if it is not a valid input we let the user know then we re-request an input)
+#If player reaches 3,3 we print out "victory!", and then exit the program
