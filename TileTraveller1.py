7d22e26b4b0d1a3dd4c3d0a395e9df9551142abd
Birgir Steinn Ellingsen <birgirsteinnellingsen@Birgirs-MacBook-Pro.local>
2018-09-18 11:22:40 +0000

    after while oklarað

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -4,51 +4,53 @@
 #Respond to that input.(if it is not a valid input we let the user know then we re-request an input)
 #If player reaches 3,3 we print out "victory!", and then exit the program
 
-for i in range(1, 4):
-    for j in range(1, 4):
-        print(i,j)
+
 x=1
 y=1
 player_start = (x,y)
 
 player_location = player_start
 
+while player_location != (3,1):
+    if player_location == (1,1) or player_location == (2,1):
+        print("You can travel: (N)orth.")
+        valid_direction = "n" or "N"
+        direction = str(input("Directions: "))
+    elif player_location == (1,2):
+        print("You can travel: (N)orth or (S)outh or (W)est.")
+        valid_direction = "n" or "s" or "w" or "N" or "S" or "W"
+        direction = str(input("Directions: "))
+    elif player_location == (2,2) or player_location == (3,3):
+        print("You can travel: (S)outh or (W)est.")
+        valid_direction = "s" or "w" or "S" or "W"
+        direction = str(input("Directions: "))
+    elif player_location == (1,3):
+        print("You can travel: (S)outh or (E)ast. ")
+        valid_direction = "s" or "w" or "S" or "W"
+        direction = str(input("Directions: "))
+    elif player_location == (2,3):
+        print("You can travel: (E)ast or (W)est.")
+        valid_direction = "e" or "w" or "E" or "W"
+        direction = str(input("Directions: "))
+    elif player_location == (3,2):
+        print("You can travel: (N)orth or (S)outh.")
+        valid_direction = "n" or "s" or "N" or "S"
+        direction = str(input("Directions: "))
 
-if direction == str("n") or direction
-
-if player_location == (1,1) or player_location == (2,1):
-    print("You can travel: (N)orth.")
-    direction = str(input("Directions: "))
-elif player_location == (1,2):
-    print("You can travel: (N)orth or (S)outh or (W)est.")
-    direction = str(input("Directions: "))
-elif player_location == (2,2) or player_location == (3,3):
-    print("You can travel: (S)outh or (W)est.")
-    direction = str(input("Directions: "))
-elif player_location == (1,3):
-    print("You can travel: (S)outh or (E)ast. ")
-    direction = str(input("Directions: "))
-elif player_location == (2,3):
-    print("You can travel: (E)ast or (W)est.")
-    direction = str(input("Directions: "))
-elif player_location == (3,2):
-    print("You can travel: (N)orth or (S)outh.")
-    direction = str(input("Directions: "))
-elif player_location == (3,1):
+    if (direction == str("n") or direction == str("N")) and (valid_direction == "n" or valid_direction == "N"):
+        player_location = (x+0,y+1)
+    elif (direction == str("s") or direction == str("S")) and (valid_direction == "s" or valid_direction == "S"):
+        player_location = (x+0,y-1)
+    elif (direction == str("e") or direction == str("E")) and (valid_direction == "e" or valid_direction == "E"):
+        player_location = (x+1,y+0)
+    elif (direction == str("w") or direction == str("W")) and (valid_direction == "w" or valid_direction == "W"):
+        player_location = (x-1,y+0)
+    else:
+        print("Not a valid direction!")
+        direction = str(input("Directions: "))
+else:
     print("Victory!")
     exit
-
-if direction == str("n") or direction == str("N"):
-    player_location = (x+0,y+1)
-elif direction == str("s") or direction == str("S"):
-    player_location = (x+0,y-1)
-elif direction == str("e") or direction == str("E"):
-    player_location = (x+1,y+0)
-elif direction == str("w") or direction == str("W"):
-    player_location = (x-1,y+0)
-else:
-    print("Not a valid direction!")
-    direction = str(input("Directions: "))
 
 
 
